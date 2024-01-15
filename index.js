a0fe66e5ca7e7ac867d833ccb5dfed13bcb4c96b
vansh saluja <vanshsaluja200@gmail.com>
2024-01-15 03:29:19 -0400

    changed app.js's name to index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = 8080;
 const path = require('path');
 const app = express();
 // General router - Till Assignment 2
-const router = require('./router/index')
+const router = require('./router/api')
 
 // Assignment 3 routing
 const artistRouter = require('./router/artist/artist');
@@ -21,13 +21,13 @@ const staticPath = path.join(__filename, '../_FrontendStarterFiles');
 // / after / we can just name our file and it will run that file
 app.use(express.static(staticPath))
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(router);
 app.use('/api/artists', artistRouter);
 app.use('/api/albums', albumRouter);
 app.use('/api/tracks', trackRouter);
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Server running on Port", PORT);
 })
